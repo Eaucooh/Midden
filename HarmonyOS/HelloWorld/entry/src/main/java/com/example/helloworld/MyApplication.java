302e88aeb80b07409c3a33728e7eb9c849c4afd6
常青园晚 <50760269+Dynesshely@users.noreply.github.com>
2022-02-08 21:43:19 +0800

    Add files via upload

---
+++ HarmonyOS/HelloWorld/entry/src/main/java/com/example/helloworld/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
