5fd46b6b13910b40d6773ef3f498d27657ede859
常青园晚 <50760269+Dynesshely@users.noreply.github.com>
2022-02-08 23:28:33 +0800

    Add files via upload

---
+++ C#实例项目/AeroHotKey - 全局热键实现/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+    
+        }
+        int alts,altd;
+        private void Init()
+        {
+            alts = HotKey.GlobalAddAtom("Alt-S");
+            altd = HotKey.GlobalAddAtom("Alt-D");
+            HotKey.RegisterHotKey(this.Handle, alts, HotKey.KeyModifiers.Alt, (int)Keys.S);
+            HotKey.RegisterHotKey(this.Handle, altd, HotKey.KeyModifiers.Alt, (int)Keys.D);
+            if (DwmApi.DwmIsCompositionEnabled())
+            {
+                DwmApi.DwmExtendFrameIntoClientArea(this.Handle, new DwmApi.MARGINS(-1, -1, -1, -1));
+            }
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+            if (DwmApi.DwmIsCompositionEnabled())
+            {
+                e.Graphics.Clear(Color.Black);
+            }
+        } 
+        protected override void WndProc(ref Message m)// 监视Windows消息
+        {
+            switch (m.Msg)
+            {
+                case HotKey.WM_HOTKEY:
+                    ProcessHotkey(m);//按下热键时调用ProcessHotkey()函数
+                    break;
+            }
+            base.WndProc(ref m); //将系统消息传递自父类的WndProc
+        }
+
+         private void ProcessHotkey(Message m) //按下设定的键时调用该函数
+        {
+            IntPtr id = m.WParam;//IntPtr用于表示指针或句柄的平台特定类型
+             int sid=id.ToInt32();
+            if(sid==alts)
+            {
+                MessageBox.Show("按下Alt+S");
+            }
+            else if(sid==altd)
+            {
+                MessageBox.Show("按下Alt+D");
+            }
+        }
+
+
+         bool IMessageFilter.PreFilterMessage(ref Message m)
+         {
+             const int WM_HOTKEY = 0x0312;//如果m.Msg的值为0x0312那么表示用户按下了热键
+             switch (m.Msg)
+             {
+                 case WM_HOTKEY:
+                     ProcessHotkey(m);//按下热键时调用ProcessHotkey()函数
+                     break;
+             }
+             //如果筛选消息并禁止消息被调度，则为 true；如果允许消息继续到达下一个筛选器或控件，则为 false
+             return false ;
+         }
+
+         private void button1_Click(object sender, EventArgs e)
+         {
+             DwmApi.DwmEnableComposition(false);
+         }
+
+    }
+}
---
+++ C#实例项目/AeroHotKey - 全局热键实现/WpfApplication1/App.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+
+      
+        #region
+        //private struct KBDLLHOOKSTRUCT
+        //{
+        //    public int vkCode;
+        //    int scanCode;
+        //    public int flags;
+        //    int time;
+        //    int dwExtraInfo;
+        //}
+
+        //private delegate int LowLevelKeyboardProcDelegate(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        //[DllImport("user32.dll")]
+        //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod, int dwThreadId);
+
+        //[DllImport("user32.dll")]
+        //private static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        //[DllImport("user32.dll")]
+        //private static extern int CallNextHookEx(int hHook, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        //[DllImport("kernel32.dll")]
+        //private static extern IntPtr GetModuleHandle(IntPtr path);
+
+        //private IntPtr hHook;
+        //LowLevelKeyboardProcDelegate hookProc; // prevent gc
+        //const int WH_KEYBOARD_LL = 13;
+
+        //public App()
+        //{
+        //    // hook keyboard
+        //    IntPtr hModule = GetModuleHandle(IntPtr.Zero);
+        //    hookProc = new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc);
+        //    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hModule, 0);
+        //    if (hHook == IntPtr.Zero)
+        //    {
+        //        MessageBox.Show("Failed to set hook, error = " + Marshal.GetLastWin32Error());
+        //    }
+        //}
+
+        //protected override void OnExit(ExitEventArgs e)
+        //{
+        //    UnhookWindowsHookEx(hHook); // release keyboard hook
+        //    base.OnExit(e);
+        //}
+
+        //private static int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        //{
+        //    if (nCode >= 0)
+        //        switch (wParam)
+        //        {
+        //            case 256: // WM_KEYDOWN
+        //            case 257: // WM_KEYUP
+        //            case 260: // WM_SYSKEYDOWN
+        //            case 261: // M_SYSKEYUP
+        //                if (
+        //                    (lParam.vkCode == 0x09 && lParam.flags == 32) || // Alt+Tab
+        //                    (lParam.vkCode == 0x1b && lParam.flags == 32) || // Alt+Esc
+        //                    (lParam.vkCode == 0x73 && lParam.flags == 32) || // Alt+F4
+        //                    (lParam.vkCode == 0x1b && lParam.flags == 0) || // Ctrl+Esc
+        //                    (lParam.vkCode == 0x5b && lParam.flags == 1) || // Left Windows Key 
+        //                    (lParam.vkCode == 0x5c && lParam.flags == 1))    // Right Windows Key 
+        //                {
+        //                    return 1;
+        //                }
+        //                break;
+        //        }
+        //    return CallNextHookEx(0, nCode, wParam, ref lParam);
+        //}
+        #endregion
+    }
+}
