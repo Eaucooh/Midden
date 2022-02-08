e88d6e9654b671b7efda14cc30ff521ea485e476
常青园晚 <50760269+Dynesshely@users.noreply.github.com>
2022-02-08 22:04:50 +0800

    Add files via upload

---
+++ 文本对比/Src/CompareV2/YD.Compare/DetailUnit.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YD.Compare
+{
+    public struct DetailUnit
+    {
+        public string UnitContent;
+        public CompareState UnitState;
+    }
+}
