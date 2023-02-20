d4ff42a3acc0d62e54067d3f7fc4b415da9de3ca
Dynesshely <catrol@qq.com>
2023-02-20 21:17:05 +0800

    💾 Feat: 万年历

---
+++ Projects/Winform/CsharpCalendar/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace calendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.lbl12 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "阳历";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(105, 45);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(11, 12);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "1";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(105, 203);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(41, 12);
+            this.lbl6.TabIndex = 3;
+            this.lbl6.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "节气";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(105, 170);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(41, 12);
+            this.lbl5.TabIndex = 5;
+            this.lbl5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "节日";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(105, 231);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(41, 12);
+            this.lbl7.TabIndex = 7;
+            this.lbl7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(105, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "阳历";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(105, 139);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(41, 12);
+            this.lbl4.TabIndex = 9;
+            this.lbl4.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(46, 139);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 12);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "时辰";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(105, 112);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(47, 12);
+            this.lbl3.TabIndex = 11;
+            this.lbl3.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(46, 112);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 12);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "农历";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(105, 76);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(47, 12);
+            this.lbl2.TabIndex = 13;
+            this.lbl2.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(46, 76);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(29, 12);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "属相";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(10, 231);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(65, 12);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "前一个节气";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(46, 376);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(29, 12);
+            this.label16.TabIndex = 25;
+            this.label16.Text = "星座";
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.Location = new System.Drawing.Point(105, 257);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(47, 12);
+            this.lbl8.TabIndex = 24;
+            this.lbl8.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(10, 257);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(65, 12);
+            this.label18.TabIndex = 23;
+            this.label18.Text = "后一个节气";
+            // 
+            // lbl9
+            // 
+            this.lbl9.AutoSize = true;
+            this.lbl9.Location = new System.Drawing.Point(105, 284);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(47, 12);
+            this.lbl9.TabIndex = 22;
+            this.lbl9.Text = "label19";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(46, 284);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(29, 12);
+            this.label20.TabIndex = 21;
+            this.label20.Text = "干支";
+            // 
+            // lbl12
+            // 
+            this.lbl12.AutoSize = true;
+            this.lbl12.Location = new System.Drawing.Point(105, 376);
+            this.lbl12.Name = "lbl12";
+            this.lbl12.Size = new System.Drawing.Size(47, 12);
+            this.lbl12.TabIndex = 20;
+            this.lbl12.Text = "label21";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(105, 284);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(29, 12);
+            this.label22.TabIndex = 19;
+            this.label22.Text = "阳历";
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Location = new System.Drawing.Point(105, 315);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(47, 12);
+            this.lbl10.TabIndex = 18;
+            this.lbl10.Text = "label23";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(46, 348);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(29, 12);
+            this.label24.TabIndex = 17;
+            this.label24.Text = "星宿";
+            // 
+            // lbl11
+            // 
+            this.lbl11.AutoSize = true;
+            this.lbl11.Location = new System.Drawing.Point(105, 348);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(47, 12);
+            this.lbl11.TabIndex = 16;
+            this.lbl11.Text = "label25";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(46, 315);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(29, 12);
+            this.label26.TabIndex = 15;
+            this.label26.Text = "星期";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 416);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.lbl12);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label lbl12;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label label26;
+    }
+}
+
---
+++ Projects/Winform/CsharpCalendar/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime dt = DateTime.Now;
+            Calendar cc = new Calendar(dt);
+            lbl1.Text = cc.DateString;
+            lbl2.Text = cc.AnimalString;
+            lbl3.Text = cc.ChineseDateString;
+            lbl4.Text = cc.ChineseHour;
+            lbl5.Text = cc.ChineseTwentyFourDay;
+            lbl6.Text = cc.DateHoliday;
+            lbl7.Text = cc.ChineseTwentyFourPrevDay;
+            lbl8.Text = cc.ChineseTwentyFourNextDay;
+            lbl9.Text = cc.GanZhiDateString;
+            lbl10.Text = cc.WeekDayStr;
+            lbl11.Text = cc.ChineseConstellation;
+            lbl12.Text = cc.Constellation;
+            
+        }
+    }
+}
